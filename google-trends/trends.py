42e85a3a33a39deff927df230e7d0ac7e7176079
travis@jataware.com <travis@jataware.com>
2021-07-16 11:10:55 -0700

    causemosified output and fail feature

--- google-trends/trends.py
+++ google-trends/trends.py
@@ -3,6 +3,8 @@ import argparse
 import pandas as pd
 import dateutil.parser as dp
 import os
+from time import time
+import numpy as np
 
 # Example CLI: python3 trends.py --term=election --country=Ethiopia --state='Addis Ababa' --output=election.csv
 
@@ -10,16 +12,21 @@ import os
 print(os.getcwd())
 
 # Read in iso codes to build lookup dicts
-iso = pd.read_csv('iso-codes.csv')
-iso['country_code'] = iso['iso_code'].apply(lambda x: x.split('-')[0])
+iso = pd.read_csv("iso-codes.csv")
+iso["country_code"] = iso["iso_code"].apply(lambda x: x.split("-")[0])
 
 # Dict of Country to iso2 code (for country only trend search):
 ctry_to_iso_d = dict(zip(iso.countryLabel, iso.country_code))
 
 # Dict of (key=country, key=state) value = Country-state iso2 code (for country and state trend search)
-state_to_iso_d = iso.groupby('countryLabel').apply(lambda x: dict(zip(x['admin1Label'], x["iso_code"]))).to_dict()
+state_to_iso_d = (
+    iso.groupby("countryLabel")
+    .apply(lambda x: dict(zip(x["admin1Label"], x["iso_code"])))
+    .to_dict()
+)
 
-#convert user input to iso code:
+
+# convert user input to iso code:
 def input_to_iso(country, state=None):
     if state:
         geo = state_to_iso_d.get(country, {}).get(state, None)
@@ -27,73 +34,129 @@ def input_to_iso(country, state=None):
         geo = ctry_to_iso_d.get(country, None)
     return geo
 
+
 def iso_to_epoch(iso_time):
     parsed_t = dp.parse(iso_time)
-    return parsed_t.strftime('%s')
+    return parsed_t.strftime("%s")
+
+
+def no_data(country, state, term):
+    if state:
+        admin1 = state
+    else:
+        admin1 = np.nan
+
+    empty_dict = {
+        "timestamp": [round(time() * 1000)],
+        "country": [country],
+        "admin1": [admin1],
+        "feature": ["trend level"],
+        "value": [np.nan],
+        "search_term": [f"{term}"],
+    }
+
+    return pd.DataFrame(empty_dict)
+
 
 def get_trend(term, country, state):
 
     geo = input_to_iso(country, state)
-
     # geo can = None if the user enters invalid geo keys
     if geo:
-        pytrend = TrendReq(hl='en-US', tz=0, geo=geo)
+        pytrend = TrendReq(hl="en-US", tz=0, geo=geo)
         pytrend.build_payload(kw_list=[term])
         trend_df = pytrend.interest_over_time()
 
-        # NO Google Trend
-        if trend_df.shape[0] == 0:
-            print(f"\nNo trend available for term:'{term}' country:'{country}' state:'{state}'\n")
-            return None
+        if trend_df.empty:
+            print(
+                f"\nNo trend available for term:'{term}' country:'{country}' state:'{state}'\n"
+            )
+            return no_data(country, state, term)
 
         # YES Google trend
         else:
             trend_df = trend_df.reset_index()
-            trend_df = trend_df.rename(columns={'date': 'time', term: 'value'})
-            trend_df = trend_df.drop(columns='isPartial')
-            trend_df['description'] = f'Google Trends for the term {term} for the geography {geo}'
-            trend_df['country'] = country
-            trend_df['iso2'] = geo[:2]
-            
+            trend_df = trend_df.rename(columns={"date": "time", term: "value"})
+            trend_df = trend_df.drop(columns="isPartial")
+            trend_df["feature"] = "trend level"
+            trend_df["country"] = country
+            trend_df["search_term"] = f"{term}"
+
             if state:
-                trend_df['state'] = state
+                trend_df["admin1"] = state
 
             else:
-                trend_df['state'] = None
+                trend_df["admin1"] = np.nan
 
             # Convert to epoch time
             trend_df["timestamp"] = trend_df.time.apply(lambda x: iso_to_epoch(str(x)))
+            trend_df = trend_df.drop(columns="time")
 
             # reorder/rename columns and keep ISO time for comparison
             cols = list(trend_df)
-            cols.insert(0, cols.pop(cols.index('timestamp')))
-            trend_df = trend_df.reindex(columns= cols) 
-            trend_df.rename(columns={'time': 'iso_time'}, inplace=True)
+            cols.insert(0, cols.pop(cols.index("timestamp")))
+            trend_df = trend_df.reindex(columns=cols)
 
-            return trend_df
+            df = pd.DataFrame(
+                trend_df,
+                columns=[
+                    "timestamp",
+                    "country",
+                    "admin1",
+                    "feature",
+                    "value",
+                    "search_term",
+                ],
+            )
+
+            return df
 
     # bad user country/state inputs
     else:
         print(f"\nFor country:'{country}' state:'{state}'")
-        print("Invalid input:\n  Reference 'iso_codes.csv' for country/state names.\n  Note: Input parameters with spaces must be in quotes.\n")
-        return None
+        print(
+            "Invalid input:\n  Reference 'country_admin1.csv' for country/state names.\n  Note: Input parameters with spaces must be in quotes.\n"
+        )
+
+        return no_data(country, state, term)
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser('Obtain Google Trends data')
-    parser.add_argument('--term', dest ='term', type=str, help='The term of interest.')
-    parser.add_argument('--country', dest ='country', type=str, default='Ethiopia', help='Required: Country of interest: "Ethiopia"')
-    parser.add_argument('--state', dest ='state', type=str, default=None, help='Optional: State/province/woreda of interest: "Addis Ababa"')
-    parser.add_argument('--output', dest ='output', type=str, default='trend.csv', help='The name of the desired output file.')
+    parser = argparse.ArgumentParser("Obtain Google Trends data")
+    parser.add_argument("--term", dest="term", type=str, help="The term of interest.")
+    parser.add_argument(
+        "--country",
+        dest="country",
+        type=str,
+        default="Ethiopia",
+        help='Required: Country of interest: "Ethiopia"',
+    )
+    parser.add_argument(
+        "--state",
+        dest="state",
+        type=str,
+        default=None,
+        help='Optional: State/province/woreda of interest: "Addis Ababa"',
+    )
+    parser.add_argument(
+        "--output",
+        dest="output",
+        type=str,
+        default="trend.csv",
+        help="The name of the desired output file.",
+    )
     args = parser.parse_args()
 
     term = args.term
     country = args.country
     state = args.state
     output = args.output
-
+    print(state)
+    print(type(state))
     trend = get_trend(term, country, state)
 
     if isinstance(trend, pd.DataFrame):
         trend.to_csv(args.output, index=False)
+        print(trend)
         print(f'\n"{output}" written to {os.getcwd()}\n')
